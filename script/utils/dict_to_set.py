ba4b7d1c92ec9a27517d4d86fc39d181c6f2f6c6
NiShadrin <nishadrin@gmail.com>
2023-03-05 20:53:47 +0300

    fix typing, renaming and add type exception

--- script/utils/dict_to_set.py
+++ script/utils/dict_to_set.py
@@ -1,32 +1,32 @@
-from typing import List, Dict, Set
+from typing import List, Dict, Set, Union
 
 
-class DictionaryParser:
+class DictionaryToSet:
     """
     Convert dictionary to set.
     """
-    __slots__ = ['result']
+    __slots__ = ['data']
 
     def __init__(self) -> None:
-        self.result = set()
+        self.data = set()
 
-    def add_to_set(self, data: Dict) -> Set:
+    def add_to_set(self, dictionary: Dict) -> Set:
         """
         Add dictionary to set and return it.
-        :param data: Dict
-        :return: self.result: List
+        :param dictionary: Dict
+        :return: self.data: Set
         """
-        self.add_or_next(data)
-        return self.result
+        self.add_or_next(dictionary)
+        return self.data
 
-    def clear_result(self):
-        self.result = set()
+    def clear_data(self) -> None:
+        self.data = set()
 
-    def __add_item(self, item) -> None:
-        self.result.add(item)
+    def __add_item(self, item: Union[str, int]) -> None:
+        self.data.add(item)
 
-    def __add_dict(self, data: Dict) -> None:
-        for key, value in data.items():
+    def __add_dict(self, dictionary: Dict) -> None:
+        for key, value in dictionary.items():
             self.__add_item(key)
             self.add_or_next(value)
 
@@ -34,15 +34,19 @@ class DictionaryParser:
         for key in data:
             self.add_or_next(key)
 
-    def add_or_next(self, data: Dict) -> None:
+    def add_or_next(self, dictionary: Dict) -> None:
         """
-        Add dictionary to result.
-        :param data:
+        Add dictionary to self.data.
+        :param dictionary: Dict
         :return:
         """
-        if isinstance(data, dict):
-            self.__add_dict(data)
-        elif isinstance(data, list):
-            self.__add_list(data)
+        if isinstance(dictionary, dict):
+            self.__add_dict(dictionary)
+        elif isinstance(dictionary, list):
+            self.__add_list(dictionary)
+        elif isinstance(dictionary, int) or isinstance(dictionary, str):
+            self.__add_item(dictionary)
         else:
-            self.__add_item(data)
+            raise Exception(
+                f'Wrong input data type. Awaited: dict, list, str or int. Get - '
+                f'{type(dictionary)}')
